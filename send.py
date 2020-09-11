ebfff526051aa561481c6be47dace5280888ec25
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-09-11 21:54:29 +0100

    fixed py

--- send.py
+++ send.py
@@ -11,7 +11,7 @@ def zipdir(path, ziph):
 if __name__ == '__main__':
     zipfile.ZipFile('build.zip', mode='w').write("build.bin")
     with open('build.zip', 'rb') as f:
-        r = ('http://teetodo.herokuapp.com/api/file', files={'file': f})
+        r = ('http://teetodo.herokuapp.com/api/file', {'file': f})
 
 
     
