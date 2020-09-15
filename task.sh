665b3afeb8f378d14fa0ead79640f789338e8393
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-09-15 22:14:38 +0100

    added flaf

--- task.sh
+++ task.sh
@@ -1,8 +1,10 @@
-git clone https://github.com/teezzan/blink_idf.git
-cd ./blink_idf
+# git clone https://github.com/teezzan/blink_idf.git
+GIT_REPO=$1
+GIT_REPO_NAME=$2
+git clone $GIT_REPO
+cd ./$GIT_REPO_NAME
 idf.py build
 cd ./build
 zip bin.zip *.bin
 curl -F "file=@bin.zip" https://file.io
 exit
-
